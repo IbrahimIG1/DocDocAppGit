7514ccc3f0b1273b6d46b9cd923c7d8e3b5fc965
Ibrahim Algammal <ibrahim13elgammal@gmail.com>
2024-05-11 14:40:44 +0300

    - added product & develpment flavors for android - change android app name for each flavors - added launch.json flavor config -  added two main files for each flavors

--- lib/main_production.dart
+++ lib/main_production.dart
@@ -1,10 +1,13 @@
 import 'package:doc_doc_app/core/Routing/app_router.dart';
 import 'package:doc_doc_app/core/di/dependency_injection.dart';
 import 'package:doc_doc_app/doc_app.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
   setupGetIt();
+  //  to fix text hiddin error in release mode
+  await ScreenUtil.ensureScreenSize();
   runApp(DocApp(
     appRouter: AppRouter(),
   ));
